62202b25f04ee625fd600e580c7d90533e22bf63
theobontempelli <theobtp@gmail.com>
2016-02-03 15:22:28 +0100

    Structure initiale

--- Serveur.java
+++ Serveur.java
@@ -1,5 +1,29 @@
 public class Serveur {
+	int capacite;
+	int taille;
+	int ligne;
+	int emplacement;
+	Pool p;
 	
-	prout!!
-	Bonjour
+	public Serveur(int c,int t){
+		capacite=c;
+		taille=t;
+		ligne=-1;
+		emplacement=-1;
+		p=null;
+	}
+	
+	int getCapacite(){return capacite;}
+	void setCapacite(int c){capacite=c;}
+	int getTaille(){return taille;}
+	void setTaille(int c){taille=c;}
+	int getLigne(){return ligne;}
+	void setLigne(int c){ligne=c;}
+	int getEmplacement(){return emplacement;}
+	void setEmplacement(int c){emplacement=c;}
+	Pool getPool(){return p;}
+	void setPool(Pool pool){p=pool;}
+	
+	
+	
 }
