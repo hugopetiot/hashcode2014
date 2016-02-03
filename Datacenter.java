aba3d22ca4acdbcd7ef67c53e7040c0040cd40e1
Hugo <hugo.petiot@gmail.com>
2016-02-03 15:52:41 +0100

    Constructeur Datacenter

--- Datacenter.java
+++ Datacenter.java
@@ -1,12 +1,20 @@
 public class Datacenter{
 	int rangees, emplacements, emplacementsIndisponibles, groupes, serveurs;
-	int[][] data;
+	Serveur[][] data;
 	
 	
 	public Datacenter(int r, int e, int ei, int g, int s){
 		rangees = r;
 		emplacements = e;
 		emplacementsIndisponibles = ei;
+		groupes = g;
+		serveurs = s;
+		data = new Serveur[rangees][emplacements];
+		for(int i=0;i<rangees;i++){
+			for(int j=0;i<emplacements;i++){
+				data[i][j] = null;
+			}
+		}
 	}
 	
 	int getRangees(){
