5de6d0a412246ae3784edd0b109a1b6823985bc6
Djczq <jeromedjczq@gmail.com>
2016-02-03 16:21:07 +0100

    modif main

--- Main.java
+++ Main.java
@@ -1,4 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
+	static Scanner in = new Scanner(System.in);
 
+	public Datacenter loadData(File f){
+		Datacenter datac = null;		
+		int rangees, emplacements, emplacementsIndisponibles, groupes, serveurs;
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			in = new Scanner(fis);
+			//2rangéesde5emplacements,1emplacementindisponible,2groupeset5serveurs.
+			rangees = in.nextInt();
+			emplacements= in.nextInt(); 
+			emplacementsIndisponibles= in.nextInt(); 
+			groupes= in.nextInt(); 
+			serveurs= in.nextInt();
+			
+			datac = new Datacenter(rangees, emplacements, emplacementsIndisponibles, groupes, serveurs);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return datac;
+	}
 }
